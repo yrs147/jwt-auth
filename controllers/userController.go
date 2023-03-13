9e5dd4472cb8ac30d6b3dacb88154f3247904168
yrs147 <yashraj14700728@gmail.com>
2023-03-13 23:00:34 +0530

    Created structure for the controller

---
+++ controllers/userController.go
@@ -0,0 +1,31 @@
+package controllers
+
+import(
+	"context"
+	"fmt"
+	"log"
+	"strconv"
+	"net/http"
+	"time"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	helper "jwt-auth/helpers"
+	"jwt-auth/models"
+	"jwt-auth/helpers"
+	"golang.org/crypto/bcrypt"
+)
+var userCollection *mongo.Collection = database.OpenCollection(database.Client,"user")
+var validate = validator.New()
+
+
+func HashPassword()
+
+func VerifyPassword()
+
+func Signup()
+
+func Login()
+
+func GetUsers()
+
+func GetUser()
