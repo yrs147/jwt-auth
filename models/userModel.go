af650ad966a1bdc0ced76e2965fdf972a631c530
yrs147 <yashraj14700728@gmail.com>
2023-03-13 22:11:49 +0530

    Added user struct

--- models/userModel.go
+++ models/userModel.go
@@ -5,14 +5,14 @@ import (
 )
 
 type User struct {
-	ID			primitive		`bson: "_id"`
-	username	*string			`json:"first_name" validate:"re"`
-	Password	
-	Email
-	Token
-	User_type
-	Refresh_token
-	Created_at
-	Updated_at
-	User_id
+	ID				primitive		`bson: "_id"`
+	Username		*string			`json:"username" validate:"required, min=2, max=100"`
+	Password		*string			`json:"Password" validate:"required, min=6"`
+	Email			*string			`json:"email" validate:"email, required"`
+	Token			*string			`json: "token"`
+	User_type		*string			`json:"user_type" validate:"required, eq=ADMIN|eq=USER"`
+	Refresh_token	*string			`json:"refresh_token"`
+	Created_at		time.Time 		`json:"created_at"`
+	Updated_at		time.Time		`json:"update_at"`
+	User_id			string			`json:"user_id"`
 }
