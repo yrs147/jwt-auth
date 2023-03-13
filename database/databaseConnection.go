7a1bdbdd15667d140687c75eec5b85d869ab96e3
yrs147 <yashraj14700728@gmail.com>
2023-03-13 22:51:36 +0530

    Added Database connection function

---
+++ database/databaseConnection.go
@@ -0,0 +1,42 @@
+package database
+
+import(
+	"fmt"
+	"time"
+	"log"
+	"os"
+	"context"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBinstance() *mongoClient{
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	MongoDb := os.Getenv("MONGODB_DB")
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDb))
+	if err !=nil {
+		log.Fatal(err)
+	}
+
+	context.WithTimeout(context.Background(), 10*time.Second) 
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to Mongo")
+
+	return Client
+}
+
+var Client *mongo.client = DBinstance()
+
+func OpenCollection(client *mongodb.Client, collectionName string) *mongoCollection{
+	var collection *mongo.Collection = client.Database("cluster0").Collection(collectName)
+	return collection
+}
--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	controller "jwt-auth/controllers"
+	"jwt-auth/middleware"
+	"github.com/gin-gonic/gin"
+
+)
+func UserRoutes(incomingRoutes *gin.Engine){
+	incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.GET("/users", controller.GetUsers())
+	incomingRoutes.GET("/users/:user_id", controller.GetUser())
+}
