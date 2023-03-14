57d6430854fa22835d170e1db5eb32a0ab1223b0
yrs147 <yashraj14700728@gmail.com>
2023-03-14 22:00:28 +0530

    Created the signup fucntion and added validation function

--- controllers/userController.go
+++ controllers/userController.go
@@ -22,7 +22,34 @@ func HashPassword()
 
 func VerifyPassword()
 
-func Signup()
+func Signup() gin.HandlerFunc{
+	return func(c *gin.Context){
+		var ctx , cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
+
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		validationErr := validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadrequest, gin.H{"error": validateErr.Error()})
+		}
+
+		count , err := userColletion.CountDocuments(ctx ,bson.M{"email":user.Email})
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error":"error occured while checking for the Email "})
+		}
+
+		if count >0 {
+			c.JSON(http.StatusInternalServerError, gin.H{"error":"this email already exists"} )
+		}
+
+	}
+}
 
 func Login()
 
@@ -34,7 +61,18 @@ func GetUser() gin.HandlerFunc{
 
 		if err := helper.MatchUserTypeToUid(c, userId); if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
-			
+			return
 		}
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		var user models.User 
+		err := userCollection.FindOne(ctx, bson.M{"user_id":userId}).Decode(&user)
+		defer cancel()
+		if err !=nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOk, user)
+
 	}
 }
