792960752e6cd5eab3e5535e538815447da8ae01
yrs147 <yashraj14700728@gmail.com>
2023-03-21 23:11:13 +0530

    Fixed Errors

--- models/userModel.go
+++ models/userModel.go
@@ -1,19 +1,20 @@
 package models
 
 import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type User struct {
-	ID				primitive		`bson: "_id"`
-	Username		*string			`json:"username" validate:"required, min=2, max=100"`
-	Password		*string			`json:"Password" validate:"required, min=6"`
-	Email			*string			`json:"email" validate:"email, required"`
-	Token			*string			`json: "token"`
-	User_type		*string			`json:"user_type" validate:"required, eq=ADMIN|eq=USER"`
-	Refresh_token	*string			`json:"refresh_token"`
-	Created_at		time.Time 		`json:"created_at"`
-	Updated_at		time.Time		`json:"update_at"`
-	User_id			string			`json:"user_id"`
+	ID            primitive.ObjectID `bson: "_id"`
+	Username      *string            `json:"username" validate:"required,min=2,max=100"`
+	Password      *string            `json:"Password" validate:"required,min=6"`
+	Email         *string            `json:"email" validate:"email,required"`
+	Token         *string            `json: "token"`
+	User_type     *string            `json:"user_type" validate:"required,eq=ADMIN|eq=USER"`
+	Refresh_token *string            `json:"refresh_token"`
+	Created_at    time.Time          `json:"created_at"`
+	Updated_at    time.Time          `json:"update_at"`
+	User_id       string             `json:"user_id"`
 }
